f63f55e0fbd2b1d76dce935e3d32223c9fd509a4
blopa <montenegro.pablo@outlook.com>
2018-03-25 20:08:04 -0300

    finish up selector component

--- src/app/components/Selector.js
+++ src/app/components/Selector.js
@@ -4,10 +4,64 @@ import PropTypes from 'prop-types';
 import '../styles/Selector.css';
 
 export class Selector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.onFocusOut = this.onFocusOut.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.state = {
+      data: [],
+    };
+  }
+  onChange(e) {
+    let data = [];
+    if (e.target.value) {
+      data = this.props.onChange(e.target.value);
+    }
+    this.setState({
+      data: data
+    });
+  }
+  onClick(e) {
+    this.props.onClick(e.target.value);
+  }
+  onFocusOut() {
+    this.setState({
+      data: []
+    });
+  }
+  onFocus(e) {
+    if (e.target.value) {
+      this.setState({
+        data: this.props.onChange(e.target.value)
+      });
+    }
+  }
   render() {
     return (
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          className={this.props.classNames.input}
+          onChange={this.onChange}
+          onBlur={this.onFocusOut}
+          onFocus={this.onFocus}
+        />
+        {this.state.data.length > 0 ? (
+          <ul className={this.props.classNames.list}>
+            {this.state.data.map(function (data, key) {
+              return (
+                <li
+                  onClick={this.onClick}
+                  className={this.props.classNames.list_item}
+                  key={key}
+                  value={data.value}
+                >{data.title}</li>
+              )
+            }.bind(this))}
+          </ul>
+        ) : null}
       </div>
     );
   }
@@ -15,4 +69,7 @@ export class Selector extends React.Component {
 
 Selector.propTypes = {
   // https://reactjs.org/docs/typechecking-with-proptypes.html
+  classNames: PropTypes.object,
+  onClick: PropTypes.func,
+  onChange: PropTypes.func
 };
--- src/app/containers/routes/CreateList.js
+++ src/app/containers/routes/CreateList.js
@@ -7,14 +7,49 @@ import {Selector} from '../../components/Selector';
 export class CreateList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      classNames: {
+        input: 'form-control',
+        list: 'list-group',
+        list_item: 'list-group-item'
+      }
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+  onClick(value) {
+    debugger;
+  }
+  onChange() {
+    let data = [
+      {
+        value: 1,
+        title: 'Nome 1'
+      },
+      {
+        value: 2,
+        title: 'Nome 2'
+      },
+      {
+        value: 3,
+        title: 'Nome 3'
+      },
+      {
+        value: 4,
+        title: 'Nome 4'
+      }
+    ];
+    return data;
   }
   render() {
     return (
       <Main>
         <div>
           <p>Create List</p>
-          <Selector/>
+          <Selector
+            classNames={this.state.classNames}
+            onChange={this.onChange}
+            onClick={this.onClick}
+          />
         </div>
       </Main>
     );
